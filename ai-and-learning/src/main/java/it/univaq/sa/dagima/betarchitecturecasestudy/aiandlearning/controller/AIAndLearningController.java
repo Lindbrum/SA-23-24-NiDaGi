63a58f660dfcc14f0754461c03889632ef6457bc
Lindbrum <ultrascati95@gmail.com>
2024-02-13 16:15:30 +0100

    Added gateway: routes for external interfaces
    Updated controllers URL paths on all microservices

--- ai-and-learning/src/main/java/it/univaq/sa/dagima/betarchitecturecasestudy/aiandlearning/controller/AIAndLearningController.java
+++ ai-and-learning/src/main/java/it/univaq/sa/dagima/betarchitecturecasestudy/aiandlearning/controller/AIAndLearningController.java
@@ -3,12 +3,14 @@ package it.univaq.sa.dagima.betarchitecturecasestudy.aiandlearning.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ai-and-learning")
 public class AIAndLearningController {
 	
-	@GetMapping("/")
+	@GetMapping
 	public ResponseEntity<String> helloWorld() {
 		return new ResponseEntity<String>("Hello world!", HttpStatus.OK);
 	}
--- attraction-review/src/main/java/it/univaq/sa/dagima/betarchitecturecasestudy/attractionreview/controller/AttractionReviewController.java
+++ attraction-review/src/main/java/it/univaq/sa/dagima/betarchitecturecasestudy/attractionreview/controller/AttractionReviewController.java
@@ -3,12 +3,14 @@ package it.univaq.sa.dagima.betarchitecturecasestudy.attractionreview.controller
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/reviews")
 public class AttractionReviewController {
 	
-	@GetMapping("/")
+	@GetMapping
 	public ResponseEntity<String> helloWorld() {
 		return new ResponseEntity<String>("Hello world!", HttpStatus.OK);
 	}
